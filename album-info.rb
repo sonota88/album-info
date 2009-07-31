0bf033ca1c22e8b03e2230d11755d80b007d6d03
sonota <yosiot8753@gmail.com>
2009-07-31 19:24:38 +0900

    first commit

---
+++ album-info.rb
@@ -0,0 +1,320 @@
+#!/usr/bin/ruby -Ku
+
+require "rubygems"
+require "fileutils"
+require "pp"
+require "kconv"
+require "tmpdir"
+require "optparse"
+require "id3lib"
+require "ya2yaml"
+require "zipruby"
+
+$Editor = "gedit"
+$Album_info_file = "info.yaml"
+$Jamendo_readme = "Readme - www.jamendo.com .txt"
+
+$AI_template =<<EOB
+--- 
+artists: 
+- name: 
+release_url: []
+pub_date: 
+licenses: 
+- verify_at: 
+  url: 
+album: 
+  title: 
+  id: 
+EOB
+
+
+class ArchiveFile
+
+  def initialize(arc_path)
+    @arc_path = arc_path
+
+    if not File.exist?(@arc_path)
+      raise "could not find #{@arc_path}."
+    end
+  end
+
+
+  def entry_exist?(entry)
+    Zip::Archive.open(@arc_path) do |ar|
+      n = ar.num_files  
+      n.times do |i|
+        name = ar.get_name(i)
+        return true if name == entry
+      end
+    end
+    false
+  end
+
+
+  def entry_read(entry)
+    content = nil
+    Zip::Archive.open(@arc_path) do |ar|
+      ar.fopen(entry) do |f|
+        next if f.name != entry
+        content = f.read
+      end
+    end
+    content
+  end
+
+
+  def entry_add(path)
+    Zip::Archive.open(@arc_path) do |ar|
+      ar.add_file path
+    end
+  end
+
+
+  def entry_replace(path)
+    Zip::Archive.open(@arc_path) do |ar|
+      $stderr.puts ar.replace_file(0, path)
+    end
+  end
+
+
+  def entry_rm(entry)
+    Zip::Archive.open(@arc_path) do |ar|
+      n = ar.num_files
+      n.times do |i|
+        entry_name = ar.get_name(i)
+        ar.fopen(entry_name) do |f|
+          if f.name == entry
+            f.delete()
+          end
+        end
+      end
+    end
+  end
+
+
+  def entry_cp(entry, newentry)
+    content = nil
+    Zip::Archive.open(@arc_path) do |ar|
+      n = ar.num_files
+      ar.fopen(entry) do |f|
+        next if f.name != entry
+        content = f.read
+      end
+      ar.add_buffer(newentry, content)
+    end
+  end
+
+
+  def entry_mv(entry, newentry)
+    entry_cp(entry, newentry)
+    entry_rm(entry)
+  end
+
+
+  def entry_list
+    result = []
+    Zip::Archive.open(@arc_path) do |ar|
+      n = ar.num_files
+      n.times do |i|
+        result << ar.get_name(i)
+      end
+    end
+    result
+  end
+end
+
+
+def kconv_u16tou8(str)
+  Kconv.kconv( str, Kconv::UTF8, Kconv::UTF16)
+end
+
+
+def exec_cmd(str)
+  $stderr.puts str
+  system str
+end
+
+
+def get_release_url_old(path)
+  result = nil
+
+  case File.extname(path)
+  when /\.mp3$/i
+    tag = ID3Lib::Tag.new(path)
+    tag.each do |frame|
+      if frame[:id] == :WOAS
+        result = frame[:url]
+      end
+    end
+  end    
+  
+  result
+end
+
+
+def get_release_url(arc)
+  result = {}
+
+  entry = nil
+  ext = nil
+
+  arc.entry_list.each do |e|
+    ext = File.extname(e)
+    if /\.mp3$/ =~ ext
+      entry = e
+      break
+    end
+  end
+
+  if entry == nil
+    raise "could not find mp3."
+  end
+
+  $stderr.puts entry
+
+  temp_path = "000#{ext}"
+  open(temp_path, "wb") do |f|
+    f.write arc.entry_read(entry)
+  end
+
+  case ext
+  when /\.mp3$/i
+    tag = ID3Lib::Tag.new(temp_path)
+    tag.each do |frame|
+      case frame[:id]
+      when :WOAS
+        result[:release_url] = frame[:url]
+      when :WCOP
+        result[:license_url] = frame[:url]
+      when :TALB
+        result[:album_title] = kconv_u16tou8(frame[:text])
+      else
+        ;
+      end
+    end
+  end    
+  
+  result
+end
+
+
+
+
+class AlbumInfo
+  def initialize(arc_path)
+    @arc_path = arc_path
+    @arc = ArchiveFile.new(arc_path)
+  end
+
+
+  def album_info_template
+    info_exist = nil
+    info_valid = nil
+
+    if @arc.entry_exist? $Album_info_file
+      info_exist = true
+      # check validness
+      begin
+        YAML.load( @arc.entry_read($Album_info_file) )
+        info_valid = true
+      rescue
+        info_valid = false
+      end
+    else
+      info_exist = false
+    end
+
+    puts "info_exist: #{info_exist}"
+    puts "info_valid: #{info_valid}"
+    
+    result = {}
+
+    if info_valid
+      result = YAML.load( @arc.entry_read($Album_info_file) )
+    else
+      result = YAML.load($AI_template)
+
+      if @arc.entry_exist?($Jamendo_readme)
+        existing = get_release_url(@arc)
+        result["release_url"] << existing[:release_url]
+        result["licenses"] << {'url'=>existing[:license_url]}
+        result["album_title"] =  existing[:album_title]
+      else
+        print "no info.\n"
+      end
+    end
+
+    result
+  end
+
+
+  def new_or_modify
+    arcfile = File.basename(@arc_path)
+    Dir.chdir File.dirname(@arc_path)
+
+    temp_infopath = File.join(Dir.tmpdir, $Album_info_file)
+    open(temp_infopath, "w") {|f| f.puts album_info_template.to_yaml }
+    exec_cmd( %Q! #{$Editor} "#{temp_infopath}" ! )
+    FileUtils.cp(temp_infopath, "000.yaml") if $DEBUG
+
+    new_arcfile = "#{arcfile}_with_info.zip"
+    FileUtils.cp(arcfile, new_arcfile)
+    sleep 1
+
+    new_arc = ArchiveFile.new(new_arcfile)
+
+    if new_arc.entry_exist?($Album_info_file)
+      new_arc.entry_rm($Album_info_file)
+    end
+    new_arc.entry_add(temp_infopath)
+  end
+
+
+  def content
+    if @arc.entry_exist?($Album_info_file)
+      @arc.entry_read($Album_info_file)
+    else
+      "could not find #{$Album_info_file}"
+    end
+  end
+
+  
+  def rm
+    if @arc.entry_exist?($Album_info_file)
+      @arc.entry_rm($Album_info_file)
+    else
+      "could not find #{$Album_info_file}"
+    end
+  end
+end
+
+
+
+
+################################################################
+if $0 == __FILE__
+  opts = {}
+  ARGV.options {|o|
+    o.banner = "ruby #$0 [options] [args]"
+    o.on("-p", "--print", "print info.yaml") {|x| opts[:print] = x }
+    o.on("-r", "--remove", "remove info.yaml") {|x| opts[:remove] = x }
+    o.parse!
+  }
+
+  arc_path = nil
+  if not ARGV[0]
+    $stderr.puts "usage: #{__FILE__} foo.zip"
+    exit 1
+  end
+  arc_path = ARGV[0]
+
+  ai = AlbumInfo.new(arc_path)
+  if opts[:print]
+    puts ai.content
+  elsif opts[:remove]
+    ai.rm
+  else
+    ai.new_or_modify
+  end
+end
